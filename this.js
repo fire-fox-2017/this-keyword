566ef06af2b1642d88c378691eb7ab302cd9a900
stedyyulius <stedyyulius@gmail.com>
2017-04-03 22:44:13 +0700

    first commit

--- this.js
+++ this.js
@@ -1,11 +1,22 @@
 "use strict"
 
 // GLOBAL CONTEXT
+var stedy = this;
+console.log(this);
 
-
-
-
+this.abc = "keren"
+console.log(this.abc)
 // CLASS CONTEXT
-
+class tes {
+  constructor(testis){
+    this._testis = testis
+  }
+}
+let test = new tes("tistes")
+console.log(test.name)
 
 // RELEASE 1
+
+/* jadi this tu kaya keyword yang serbaguna buat nunjuk sesuatu di javascript basicly
+  kaya misal this bisa "Owner" dari sebuah function atau object dalam artian this dapat mewakili mereka semua
+  dan bisa menjadi object menjadi Object juga */
