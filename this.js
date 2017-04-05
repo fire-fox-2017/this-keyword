f333c4ca49f6fa98501ceb1bc91339bff77ded31
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-04-05 17:23:20 +0700

    fixing code

--- this.js
+++ this.js
@@ -1,9 +1,28 @@
 "use strict"
 
 // GLOBAL CONTEXT
+const repl = require('repl');
+console.log(repl);
+//Fungsi apa saja di dalamnya?
+//Function : Inspect;
+//Function : REPLServer;
+//Function : Interface;
+//Function : Recoverable;
+//Function : SyntaxError;
+console.log(process.versions.node);
 
+class Motor {
+  constructor(warna, model) {
+    this.warna = warna;
+    this.model = model;
+  }
 
-
+  getWarna() {
+    console.log(this.warna);
+  }
+}
+ let motor = new Motor ('red', 'x125');
+ motor.getWarna();
 
 // CLASS CONTEXT
 
