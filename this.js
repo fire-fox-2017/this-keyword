e05c2560f5a4378f66706e059062cb1214174606
ridho0 <ridho230994@gmail.com>
2017-04-05 23:48:51 +0700

    selesai

--- this.js
+++ this.js
@@ -2,10 +2,27 @@
 
 // GLOBAL CONTEXT
 
-
+// this.process.version
 
 
 // CLASS CONTEXT
 
+// class Split {
+//
+//   constructor (xxxx) {
+//     this._split = xxxx;
+//   }
+//
+//   splitThis () {
+//     return this._split.split("")
+//   }
+// }
+//
+// let split = new Split("10818887878")
+// //split.splitThis ()
+// console.log(split.splitThis())
 
 // RELEASE 1
+// this di sini biasanya merujuk ke variable globalnya.
+// kalo di dlm object this dipakai utk memanggil propertiesnya
+// di oop di js this juga bisa digunakan utk mendeklarasikan public properties
