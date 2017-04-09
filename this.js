f01323bf79e73cf4b9d3a6bde534d5717c01d55b
1000pilar <nunua7a8@gmailcom>
2017-04-09 11:54:03 +0700

    this-keywords

--- this.js
+++ this.js
@@ -1,11 +1,26 @@
 "use strict"
 
 // GLOBAL CONTEXT
+const repl = require(`repl`);
 
+console.log(repl);
+
+console.log(process.versions.node);
 
 
 
 // CLASS CONTEXT
+class Bunga{
+  constructor(jenis, warna){
+  this.jenis = jenis;
+  this.warna = warna;
+}
 
+getBunga(){
+  console.log(`Tamanku penuh ${this.jenis} berwarna ${this.warna}`)
+}
+}
 
+let bunga = new Bunga('Mawar', 'Merah')
+bunga.getBunga()
 // RELEASE 1
