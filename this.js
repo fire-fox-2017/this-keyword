e3e2905d5fc20640dc8768f0a4853ddc8d89df2b
aldyadk <aldy.andika@gmail.com>
2017-04-10 16:21:19 +0700

    komit this

--- this.js
+++ this.js
@@ -1,11 +1,15 @@
 "use strict"
 
-// GLOBAL CONTEXT
+class Sampel {
+  constructor(a,b){
+    this.key1 = a;
+    this.key2 = b;
+  }
 
+  penjumlahan() {
+    console.log(this.key1+this.key2);
+  }
+}
 
-
-
-// CLASS CONTEXT
-
-
-// RELEASE 1
+let sampel = new Sampel(1,2); //instanciate
+sampel.penjumlahan(); // memanggil method yang jg memanggil this sebagai public property dari kelas tersebut
